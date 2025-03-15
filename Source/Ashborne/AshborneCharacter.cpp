a7251f90bfc05e37d92120e3369aa8753d313a91
Naiselim <452075061@qq.com>
2025-03-15 16:10:02 +0800

    [feat][Ashborne]改为横版模式

--- Source/Ashborne/AshborneCharacter.cpp
+++ Source/Ashborne/AshborneCharacter.cpp
@@ -120,8 +120,8 @@ void AAshborneCharacter::Move(const FInputActionValue& Value)
 		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 
 		// add movement 
-		AddMovementInput(ForwardDirection, MovementVector.Y);
-		AddMovementInput(RightDirection, MovementVector.X);
+		AddMovementInput(ForwardDirection, MovementVector.X);
+	//	AddMovementInput(RightDirection, MovementVector.Y);
 	}
 }
 
